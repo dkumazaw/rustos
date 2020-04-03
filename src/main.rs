ccb32bfdcd466c0e654dad28c2f14bb1769a2f27
dkumazaw <dkumazaw@stanford.edu>
2020-04-02 22:03:33 -0700

    checkpoint

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,14 @@ mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello world!{}", "!");
-
+    panic!("Some panic message");
     loop {}
 }
