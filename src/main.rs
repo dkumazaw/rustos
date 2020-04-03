56e9414be7a246c0df72617d89546700cef9a95c
dkumazaw <dkumazaw@stanford.edu>
2020-04-02 21:59:53 -0700

    checkpoint

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,9 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello world!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {}, {}", 42, 12.22).unwrap();
+    println!("Hello world!{}", "!");
 
     loop {}
 }
