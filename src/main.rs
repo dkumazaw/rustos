05e578f68938871a634ef54106c768cde688f226
dkumazaw <dkumazaw@stanford.edu>
2020-04-04 20:24:17 -0700

    checkpoint

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn test_runner(tests: &[&dyn Fn()]) {
 #[test_case]
 fn trivial_assertion() {
     serial_print!("trivial assertion...");
-    assert_eq!(1, 1);
+    assert_eq!(1, 0);
     serial_println!("[ok]");
 }
 
@@ -41,9 +41,19 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     }
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failure);
     loop {}
 }
 
