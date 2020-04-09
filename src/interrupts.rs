56f9e1666353c16256bbb8289332c17bdacbd948
dkumazaw <dkumazaw@stanford.edu>
2020-04-08 23:26:35 -0700

    checkpoint

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -6,6 +6,7 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.double_fault.set_handler_fn(double_fault_handler);
         idt
     };
 }
@@ -17,6 +18,11 @@ pub fn init_idt() {
 extern "x86-interrupt" fn breakpoint_handler(stack_fr: &mut InterruptStackFrame)
 {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_fr);
+}
+
+extern "x86-interrupt" fn double_fault_handler(stack_fr: &mut InterruptStackFrame, _error_code: u64) -> ! 
+{
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_fr);
 }
 
 #[cfg(test)]
