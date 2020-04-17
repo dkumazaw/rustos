bb6d010685c42d0cd3cc797c092f7faa5a5944a2
dkumazaw <dkumazaw@stanford.edu>
2020-04-17 14:46:37 -0700

    checkpoint

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -17,7 +17,10 @@ macro_rules! println {
 #[doc(hidden)]
 pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
-    WRITER.lock().write_fmt(args).unwrap();
+    use x86_64::instructions::interrupts;
+
+    // Any other way to make the print statement reentrant?
+    interrupts::without_interrupts(|| {WRITER.lock().write_fmt(args).unwrap();});
 }
 
 #[allow(dead_code)]
